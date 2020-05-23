2789f97e71abb6d73c46b859b876ce32364d4fc6
Jelena Radeka <jelmil87@hotmail.com>
2020-05-23 18:35:32 +0200

    splash

---
+++ app/src/main/java/org/ftninformatika/zavrsni_ispit/SplashScreen.java
@@ -0,0 +1,57 @@
+package org.ftninformatika.zavrsni_ispit;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends Activity {
+
+    private SharedPreferences prefs;
+    private boolean splash;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        splash = prefs.getBoolean(getString(R.string.splash_key), false);
+
+        if (splash) {
+            setContentView(R.layout.activity_splash_screen);
+
+            ImageView imageView = findViewById(R.id.imageView);
+            InputStream is = null;
+            try {
+                is = getAssets().open("repertoar_logo.png");
+                Drawable drawable = Drawable.createFromStream(is, null);
+                imageView.setImageDrawable(drawable);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                    finish();
+                }
+            }, 5000);
+        } else {
+            startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            finish();
+        }
+    }
+}
+
